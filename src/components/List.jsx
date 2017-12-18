ebebb225a1d69b29ebef80f9b16e59b9a9ccf1ea
artes07 <artes07333@gmail.com>
2017-12-18 20:10:27 +0200

    done react-skeleton

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,15 +1,15 @@
-var React = require( 'react');
+var React = require('react');
 var ListItem = require('./ListItem.jsx');
 
-var ingredients = [{"id":1,"text":"ham"},{"id":2,"text":"cheese"},{"id":3,"text":"potatoes"}];
+var ingredients = [{"id":1,"text":"ham"}, {"id":2,"text":"cheese"},{"id":3,"text":"potatoes"}];
 
 var List = React.createClass({
   render: function () {
-    var listItem = ingredients.map(function (item) {
-      return <ListItem key = {item.id} ingredient = {item.text} />;
+    var listItems = ingredients.map(function(item) {
+      return <ListItem key={item.id} ingredient={item.text} />;
     });
 
-    return (<ul>{listItem}</ul>);
+    return (<ul>{listItems}</ul>);
   }
 });
 
